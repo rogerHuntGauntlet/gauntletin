dc70313ab121ff8a1f704d219fee401095dd8b1a
rogerHuntGauntlet <roger.b.hunt.iii@gauntletai.com>
2025-03-07 13:59:25 -0600

    Remove default app directory and update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // Tell Next.js where to find the pages
+  distDir: 'dist',  
 };
 
 export default nextConfig;
