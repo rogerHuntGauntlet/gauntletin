1a5b0faeec0c5cb7be0338ff9b33177e1f2f1626
rogerHuntGauntlet <roger.b.hunt.iii@gauntletai.com>
2025-03-07 14:06:15 -0600

    Fix routes manifest issue by removing custom distDir

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Tell Next.js where to find the pages
-  distDir: 'dist',  
 };
 
 export default nextConfig;
